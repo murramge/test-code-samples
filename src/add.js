482645684ccb0e6c79cc9a3a97c6f0a67a2f7f7b
kangeunhwa <dmdgi37@naver.com>
2024-06-17 04:30:13 +0900

    coverage

---
+++ src/add.js
@@ -0,0 +1,7 @@
+function add(a, b) {
+  return a - b;
+}
+
+module.exports = add;
+
+//개발단계에서 스스로 검증!
---
+++ src/test/add.test.js
@@ -0,0 +1,8 @@
+const add = require("../add.js");
+
+test("", () => {
+  // 테스트 코드 작성
+  expect(add(1, 2)).toBe(3);
+});
+
+//테스트에 관한 타입 정보가 없음 @types/jest 설치.
